6ab17c5d15bf7f4b17acd6cc2d604875792f4d6e
iulianstan <iulian1226@gmail.com>
2021-07-13 14:08:00 +0200

    Adding functions for hamburger menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,20 @@
 // Any code you will write for your website (in the future!) should go here
+// DOM selectors
+
+const menu = document.querySelector(".bars-menu");
+const sideMenu = document.querySelector(".left-side-menu");
+
+let close = false;
+
+const openCloseMenu = () => {
+  close = !close;
+  if (close) {
+    menu.classList.add("x-menu");
+    sideMenu.classList.add("close");
+  } else {
+    menu.classList.remove("x-menu");
+    sideMenu.classList.remove("close");
+  }
+};
+
+menu.addEventListener("click", openCloseMenu);
