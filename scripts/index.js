4ed4910dcacf0644f9d233c18da3addb7cb4f040
iulianstan <iulian1226@gmail.com>
2021-07-13 15:18:05 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,38 @@
 
 const menu = document.querySelector(".bars-menu");
 const sideMenu = document.querySelector(".left-side-menu");
+const body = document.querySelector("body");
+
+const switchButton = document.querySelector(".switch");
 
 let close = false;
 
 const openCloseMenu = () => {
   close = !close;
   if (close) {
-    menu.classList.add("x-menu");
+    menu.classList.remove("x-menu");
+
     sideMenu.classList.add("close");
   } else {
-    menu.classList.remove("x-menu");
+    menu.classList.add("x-menu");
     sideMenu.classList.remove("close");
   }
 };
 
+let clicked = false;
+
+const switchTo = () => {
+  clicked = !clicked;
+  if (clicked) {
+    switchButton.classList.remove("fa-toggle-off");
+    switchButton.classList.add("fa-toggle-on");
+    body.classList.add("dark");
+  } else {
+    switchButton.classList.remove("fa-toggle-on");
+    switchButton.classList.add("fa-toggle-off");
+    body.classList.remove("dark");
+  }
+};
+
 menu.addEventListener("click", openCloseMenu);
+switchButton.addEventListener("click", switchTo);
